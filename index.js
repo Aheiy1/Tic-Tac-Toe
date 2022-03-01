f0c219b2eccdfd8d0fcf575b6013ec14d30912e7
Aheiy1 <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>
2022-03-01 14:47:06 -0600

    Day 1 Changes

---
+++ index.js
@@ -0,0 +1,54 @@
+const gameState = {
+  players: ["x", "o"],
+  board: [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+};
+
+const x = document.createElement("x");
+
+const o = "o";
+let turn = 1;
+// const player1 = gameState.players[0]
+// const player2 = gameState.players[1]
+
+constboard = document.getElementById("board");
+let response = document.querySelector(".response");
+
+board.addEventListener("click", function (event) {
+  //   console.log(event.target.id.split(","));
+  if (turn === 1) {
+    response.innerHTML = "Player 2 Turn";
+    turn = 2;
+  } else if (turn === 2) {
+    response.innerHTML = "Player 1 Turn";
+    turn = 1;
+  }
+
+  //   }
+  //   response.innerHTML = "Player 2 Turn";
+
+  //   if (event.target.matches("div.cell2")){
+  //       gameState.players = [1];
+  //   }
+  //   response.innerHTML = "Player 1 Turn"
+});
+
+function gamePlay() {
+  for (let i = 0; i < gameState.board.length; i++) {
+    console.log(gameState.board[i], "loop")
+  }
+}
+function renderGame() {
+    gamePlay(); 
+  board();
+  
+}
