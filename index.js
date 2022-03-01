8012540f507f4f402d8b825a2cfa0bf85c3f453e
Aheiy1 <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>
2022-03-01 17:37:48 -0600

    X's and O's appearing on screen

--- index.js
+++ index.js
@@ -13,22 +13,24 @@ const gameState = {
   ],
 };
 
-const x = document.createElement("x");
+const circle = document.getElementsByClassName = "circle";
 
-const o = "o";
+
 let turn = 1;
 // const player1 = gameState.players[0]
 // const player2 = gameState.players[1]
 
-constboard = document.getElementById("board");
+const board = document.getElementById("board");
 let response = document.querySelector(".response");
 
 board.addEventListener("click", function (event) {
   //   console.log(event.target.id.split(","));
-  if (turn === 1) {
+  if (turn === 1 ) {
+    event.target.classList.toggle('circle')
     response.innerHTML = "Player 2 Turn";
     turn = 2;
-  } else if (turn === 2) {
+  } else if (turn === 2)  {
+    event.target.classList.toggle('x')
     response.innerHTML = "Player 1 Turn";
     turn = 1;
   }
